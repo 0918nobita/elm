064c7ceee4f1849de55b90c0c1aa2e10c6206c6a
0918nobita <nobita.0918@gmail.com>
2018-11-14 21:04:14 +0900

    :sparkles: 部分適用のサンプルを追加した

--- src/Hello.elm
+++ src/Hello.elm
@@ -3,7 +3,10 @@ module Hello exposing (..)
 import Html exposing (text)
 
 main =
-  text <| String.fromInt <| add 2 3
+  text <| String.fromInt <| add3 4
 
 add : Int -> Int -> Int
 add x y = x + y
+
+add3 : Int -> Int
+add3 = add 3
