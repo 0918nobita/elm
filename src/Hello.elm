b43541b570f4705f787b99da47f4d62b196a72c9
0918nobita <nobita.0918@gmail.com>
2018-11-15 03:02:51 +0900

    :sparkles: 無名関数, foldr, map を用いたサンプルを追加した

--- src/Hello.elm
+++ src/Hello.elm
@@ -1,12 +1,19 @@
 module Hello exposing (..)
 
 import Html exposing (text)
+import String exposing (fromInt)
+import List exposing (foldr, map)
 
 main =
-  text <| String.fromInt <| add3 4
+  text 
+  <| foldr (++) ""
+  <| map fromInt [(add3 1), (sub 7 5)]
 
 add : Int -> Int -> Int
 add x y = x + y
 
 add3 : Int -> Int
 add3 = add 3
+
+sub : Int -> Int -> Int
+sub = \x y -> x - y
