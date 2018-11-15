810dd7f74f84fccf3b0728a209e588007a2f8b22
0918nobita <nobita.0918@gmail.com>
2018-11-15 09:35:47 +0900

    :wrench: elm-format を導入した

--- src/Hello.elm
+++ src/Hello.elm
@@ -1,25 +1,35 @@
 module Hello exposing (main)
 
-import Html exposing (text)
-import String exposing (fromInt)
-import List exposing (foldr, map)
 import Debug exposing (log)
+import Html exposing (text)
+import List exposing (foldr, map)
+import String exposing (fromInt)
+
 
 main =
-  text 
-  << foldr (++) ""
-  << map fromInt
-  <| log "List" [(add3 1), (sub7 5)]
+    text
+        << foldr (++) ""
+        << map fromInt
+    <|
+        log
+            "List"
+            [ add3 1, sub7 5 ]
+
 
 add : Int -> Int -> Int
-add x y = x + y
+add x y =
+    x + y
+
 
 add3 : Int -> Int
-add3 = add 3
+add3 =
+    add 3
+
 
 sub7 : Int -> Int
 sub7 =
-  let
-    sub = \x y -> x - y
-  in
+    let
+        sub =
+            \x y -> x - y
+    in
     sub 7
